9c74cfa48e9b3f0362b97bb9b060b0c58bda5db2
Dan <DanielWRoberts@outlook.com>
2017-07-30 17:17:29 -0700

    Added some tests in mocha with supertest and expect

--- server/server.js
+++ server/server.js
@@ -2,12 +2,28 @@
 
 const express = require('express');
 
+const utils  = require('../utils/utils');
+
 let app = express();
 
 app.get("/", (req, res, next) => {
-  res.send("Hello World!");
+  res.status(404).send({error: "Page not found.", name: "Todo App"});
+});
+
+app.get("/users", (req, res, next) => {
+  res.send([
+    {
+      name: "Dude",
+      age: 23
+    }, {
+      name: "Dan",
+      age: 24
+    }
+  ]);
 });
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
+
+module.exports.app = app;
---
+++ server/server.test.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const request = require('supertest');
+const expect = require('expect');
+
+const app = require('./server').app;
+
+it("should return a hello world response", (done) => {
+  request(app)
+    .get("/")
+    .expect(404)
+    .expect((res) => {
+      expect(res.body).toInclude({error: "Page not found."});
+    })
+    .end(done);
+});
+
+it("should return my user object", (done) => {
+  request(app)
+    .get("/users")
+    .expect(200)
+    .expect((res) => {
+      expect(res.body).toInclude({name: "Dan", age: 24});
+    })
+    .end(done);
+});
